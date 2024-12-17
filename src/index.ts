554b0045e05a1c412e882e883fac0f22620dcc69
umutplvn <umutpehlivan2078@gmail.com>
2024-12-17 02:47:42 +0000

    up

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,86 @@
 //! Types
 
-// String
+//? String
 let isim: string = `Merhaba, degisiklik!`;
-console.log(isim)
+// console.log(isim)
 
-// Number
+//? Number
 let yas: number = 25;
-console.log(yas)
+// console.log(yas)
 
-// Boolean
+//? Boolean
 let isActive: boolean = true;
-console.log(isActive)
+// console.log(isActive)
 
-// Any
+//? Any
 let value: any = 42;
 value = "Merhaba";
-console.log(value); 
+// console.log(value); 
 
-// Void
+//? Void
 function selamla(): void {
     console.log("Void!");
   }
-  selamla();
+  // selamla();
   
-// Void  
+//? Void  
 let a: null = null;
 let b: undefined = undefined;
-console.log(a, b);
+// console.log(a, b);
 
-// Array
+//? Array
 let sayilar: number[] = [1, 2, 3, 4];
-console.log(sayilar); 
+// console.log(sayilar); 
 
 let karisikDizi: Array<any> = [1, "Merhaba", { isim: "Umut" }];
-console.log(karisikDizi);  // [ 1, "Merhaba", { isim: "Umut" }]
+// console.log(karisikDizi);  // [ 1, "Merhaba", { isim: "Umut" }]
 
 let karisikDizi2: (string | number | object)[] = [1, "Merhaba", { isim: "Umut" }];
-console.log(karisikDizi2);  // [ 1, "Merhaba", { isim: "Umut" }]
+// console.log(karisikDizi2);  // [ 1, "Merhaba", { isim: "Umut" }]
 
-
-// Tuple
+//? Tuple
 let kullanici: [string, number] = ["Umut", 25];
-console.log(kullanici); // ["Umut", 25]
+// console.log(kullanici); // ["Umut", 25]
 
-// Object
+//? Object
 let kisi: { isim: string, yas: number } = { isim: "Umut", yas: 25 };
-console.log(kisi); // { isim: "Umut", yas: 25 }
+// console.log(kisi); // { isim: "Umut", yas: 25 }
+
+let kisiVerisi: { isim: string, yas: number } = {
+  isim: "Umut",
+  yas: 25
+};
+
+// console.log(kisiVerisi.isim);
+// console.log(kisiVerisi.yas); 
+
+//! Functions
+
+//? Function
+function topla(a: number, b: number): number {
+  return a + b;
+}
+// console.log(topla(3, 5));
+
+//? Arrow Fucntion
+const carp=(a:number, b:number):number=>{
+  return(a*b);
+}
+let sonuc:number = carp(45, 33)
+// console.log(sonuc);
+
+
+//! Generics 
+function yazdir<T> (veri:T): void {
+  console.log(veri);
+}
+// yazdir("Merhaba, Dünya!");  
+// yazdir(100);                 
+
+function diziyiYazdir<T>(dizi: T[]): void {
+  dizi.forEach(element => console.log(element));
+}
+
+// diziyiYazdir([1, 2, 3, 4]);  
+// diziyiYazdir(["a", "b", "c"]);
+
